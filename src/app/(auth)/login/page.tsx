a614a0a6e5a46c9e1836da32a2cace6440617e07
Marmo77 <matyskamela@gmail.com>
2026-07-09 13:05:43 +0200

    Przekształcenie strony na strone dla NordScan

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,4 +1,6 @@
-import { OAuthButtons } from "@/components/auth/oauth-buttons"
+import LoginForm from "@/components/auth/login-form"
+import { OAuthButton } from "@/components/auth/oauth-button"
+import { Separator } from "@/components/ui/separator"
 import { createClient } from "@/lib/supabase/server"
 import { redirect } from "next/navigation"
 
@@ -12,19 +14,22 @@ export default async function LoginPage() {
     }
     // IN FUTURE: if user is logged in, add toast notification "Jesteś już zalogowany/a" and redirect to dashboard !
 
+
     return (
         <main className="min-h-screen bg-background flex items-center justify-center px-6">
-            <section className="w-full max-w-md rounded-2xl border bg-card p-8 shadow-sm">
-                <div className="mb-6 space-y-2 text-center">
-                    <h1 className="text-2xl font-semibold tracking-tight">
-                        Zaloguj się
-                    </h1>
-                    <p className="text-sm text-muted-foreground">
-                        Kontynuuj przez Google lub GitHub
-                    </p>
+            <section className="w-full max-w-md rounded-2xl border bg-card p-6 shadow-sm">
+                <div className="mb-2 space-y-2 text-center">
+                    <h1 className="text-2xl font-semibold tracking-tight">Zaloguj się</h1>
+                    <p className="text-sm text-muted-foreground">Kontynuuj przez Google lub Email</p>
                 </div>
-
-                <OAuthButtons />
+                <div className="flex flex-col gap-4">
+                    <LoginForm />
+                    <div className="relative">
+                        <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-background px-0.25 text-foreground/40 text-xs">OR</span>
+                        <Separator />
+                    </div>
+                    <OAuthButton />
+                </div>
             </section>
         </main>
     )
---
+++ src/components/auth/login-form.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import { FaEye, FaEyeSlash } from "react-icons/fa6"
+import { Button } from "../ui/button"
+import { Input } from "../ui/input"
+import { loginWithEmail } from "@/app/(auth)/login/actions"
+import { useActionState, useState } from "react"
+
+export default function LoginForm() {
+
+    const [state, formAction, isPending] = useActionState(loginWithEmail, null);
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [showPassword, setShowPassword] = useState(false)
+
+    return (
+        <form action={formAction}>
+            <span className="text-foreground/40 text-xs">Logowanie</span>
+            <div className="flex flex-col gap-2 py-2">
+                <Input name="email" type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+                <div className="relative">
+                    <Input name="password" type={showPassword ? "text" : "password"} placeholder="Hasło" value={password} onChange={(e) => setPassword(e.target.value)} required />
+                    <Button type={"button"} variant={"preview"} size="icon" className="absolute right-0 inset-y-0 flex items-center" onClick={() => setShowPassword(!showPassword)}>
+                        {showPassword ? <FaEyeSlash /> : <FaEye />}
+                    </Button>
+                </div>
+                <Button type="submit" variant="outline" disabled={isPending}>
+                    {isPending ? "Logowanie..." : "Zaloguj się"}
+                </Button>
+            </div>
+            {state?.error && (
+                <p className="text-xs text-red-500 text-center">{state.error}</p>
+            )}
+        </form>
+    )
+}
