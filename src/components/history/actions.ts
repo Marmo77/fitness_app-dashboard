62a695d49e6b22ae20bc23682a0b596fb61a8d32
Marmo77 <matyskamela@gmail.com>
2026-07-10 22:00:37 +0200

    Przeniesienei filtrowania + funkcjonalnosc do actions.ts

--- src/components/history/actions.ts
+++ src/components/history/actions.ts
@@ -1,3 +1,5 @@
+import { orderMockup, OrderProps } from "./orders";
+
 // Sort Options
 export const SortOptions: { value: string, label: string }[] = [
     { value: "id", label: "ID" },
@@ -13,3 +15,32 @@ export async function cancelFiltering(setSearchValue: (value: string) => void, s
     setSortValue("date");
     setFilterOrder("asc");
 }
+
+const OrderList: OrderProps[] = orderMockup;
+
+
+// Filtering and sorting Orders
+export const FilterAndSortOrders = (searchValue: string, sortValue: string, filterOrder: "asc" | "desc") => OrderList.filter((order) => {
+    const search = searchValue.toLowerCase();
+
+    return (
+        order.customerEmail.toLowerCase().includes(search) ||
+        order.customerName.toLowerCase().includes(search) ||
+        order.date.toLowerCase().includes(search) ||
+        order.id.toLowerCase().includes(search) ||
+        order.serviceName.toLowerCase().includes(search) ||
+        order.serviceType.toLowerCase().includes(search) ||
+        order.status.toLowerCase().includes(search) ||
+        (order.customerPhone && order.customerPhone.toLowerCase().includes(search))
+    );
+}).sort((a, b) => {
+    const valueA = a[sortValue as keyof OrderProps];
+    const valueB = b[sortValue as keyof OrderProps];
+
+    if (valueA === undefined) return filterOrder === "asc" ? 1 : -1;
+    if (valueB === undefined) return filterOrder === "asc" ? -1 : 1;
+
+    const comparison = String(valueA).localeCompare(String(valueB));
+
+    return filterOrder === "asc" ? comparison : comparison * -1;
+});
